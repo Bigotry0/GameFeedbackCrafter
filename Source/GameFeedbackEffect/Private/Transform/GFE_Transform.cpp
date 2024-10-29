68cb8efd9b646fa2df84c1089385dc7d56e4cb84
Bigotry0 <lim.young@foxmail.com>
2024-10-30 03:37:59 +0800

    Add a GFE of type Transform
    - Added base class and a GFE to change the target Location
    - Still in beta

---
+++ Source/GameFeedbackEffect/Private/Transform/GFE_Transform.cpp
@@ -0,0 +1,140 @@
+﻿// Copyright (c) Lim Young
+
+
+#include "Transform/GFE_Transform.h"
+
+void UGFE_TransformBase::OnInit()
+{
+	Super::OnInit();
+
+	ActorSelection.Init(GetContextType(), GetContextActor());
+}
+
+void UGFE_Transform_Location::OnPlay()
+{
+	Super::OnPlay();
+
+	if (!bValid)
+	{
+		return;
+	}
+
+	switch (TransformSpace)
+	{
+	case ETransformSpace::World:
+		if (bUseCustomTargetComponent)
+		{
+			//TODO : Implement
+		}
+		else
+		{
+			ActorSelection.GetTargetActor()->SetActorLocation(StartLocation);
+		}
+		break;
+	case ETransformSpace::Local:
+		if (bUseCustomTargetComponent)
+		{
+			//TODO : Implement
+		}
+		else
+		{
+			ActorSelection.GetTargetActor()->SetActorRelativeLocation(StartLocation);
+		}
+	default:
+		break;
+	}
+}
+
+void UGFE_Transform_Location::OnInit()
+{
+	Super::OnInit();
+
+	if (ActorSelection.IsUseCustomTargetComponent())
+	{
+		bValid = ActorSelection.IsComponentValid();
+		if (bValid)
+		{
+			bUseCustomTargetComponent = true;
+		}
+	}
+	else
+	{
+		bValid = ActorSelection.IsActorValid();
+		bUseCustomTargetComponent = false;
+	}
+}
+
+void UGFE_Transform_Location::OnStop(bool bInterrupted)
+{
+	Super::OnStop(bInterrupted);
+
+	if (!bValid)
+	{
+		return;
+	}
+
+	switch (TransformSpace)
+	{
+	case ETransformSpace::World:
+		if (bUseCustomTargetComponent)
+		{
+			//TODO : Implement
+		}
+		else
+		{
+			ActorSelection.GetTargetActor()->SetActorLocation(EndLocation);
+		}
+		break;
+	case ETransformSpace::Local:
+		if (bUseCustomTargetComponent)
+		{
+			//TODO : Implement
+		}
+		else
+		{
+			ActorSelection.GetTargetActor()->SetActorRelativeLocation(EndLocation);
+		}
+		break;
+	default:
+		break;
+	}
+}
+
+void UGFE_Transform_Location::OnTick(float DeltaTime)
+{
+	Super::OnTick(DeltaTime);
+
+	if (!bValid)
+	{
+		return;
+	}
+
+	float Alpha = GetInterpolatorAlpha();
+	FVector NewLocation = FMath::Lerp(StartLocation, EndLocation, Alpha);
+
+	switch (TransformSpace)
+	{
+	case ETransformSpace::World:
+		if (bUseCustomTargetComponent)
+		{
+			//TODO : Implement
+		}
+		else
+		{
+			ActorSelection.GetTargetActor()->SetActorLocation(NewLocation);
+		}
+		break;
+	case ETransformSpace::Local:
+		if (bUseCustomTargetComponent)
+		{
+			//TODO : Implement
+		}
+		else
+		{
+			ActorSelection.GetTargetActor()->SetActorRelativeLocation(NewLocation);
+		}
+		break;
+	default:
+		break;
+	}
+}
---
+++ Source/GameFeedbackEffect/Public/Transform/GFE_Transform.h
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Lim Young
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Common/InterpolatorGameFeedbackEffectBase.h"
+#include "GFE_Transform.generated.h"
+
+UENUM(BlueprintType)
+enum class ETransformSpace : uint8
+{
+	World,
+	Local
+};
+
+/**
+ * Base class for transform effects.
+ */
+UCLASS(Abstract)
+class GAMEFEEDBACKEFFECT_API UGFE_TransformBase : public UInterpolatorGameFeedbackEffectBase
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Transform", meta = (DisplayPriority = 0))
+	FGFActorSelection ActorSelection;
+
+	UPROPERTY(EditAnywhere, Category = "Transform", meta = (DisplayPriority = 1))
+	ETransformSpace TransformSpace = ETransformSpace::Local;
+
+	virtual void OnInit() override;
+};
+
+UCLASS()
+class UGFE_Transform_Location : public UGFE_TransformBase
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Transform|Location")
+	bool bValid = false;
+	UPROPERTY(EditAnywhere, Category = "Transform|Location")
+	bool bUseCustomTargetComponent = false;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Transform|Location")
+	FVector StartLocation;
+
+	UPROPERTY(EditAnywhere, Category = "Transform|Location")
+	FVector EndLocation;
+
+	virtual void OnPlay() override;
+	virtual void OnInit() override;
+	virtual void OnStop(bool bInterrupted) override;
+	virtual void OnTick(float DeltaTime) override;
+};
