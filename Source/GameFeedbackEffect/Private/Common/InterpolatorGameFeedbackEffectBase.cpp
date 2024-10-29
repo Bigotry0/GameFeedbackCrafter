dc9cfecfc46b746916d51caa0da0c97c48c259fd
Bigotry0 <lim.young@foxmail.com>
2024-10-30 03:35:44 +0800

    Add a GFE base class that maintains interpolators by default

---
+++ Source/GameFeedbackEffect/Private/Common/InterpolatorGameFeedbackEffectBase.cpp
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Lim Young
+
+
+#include "Common/InterpolatorGameFeedbackEffectBase.h"
+
+void UInterpolatorGameFeedbackEffectBase::OnInit()
+{
+	Super::OnInit();
+
+	Interpolator.UpdateAlpha(0);
+}
+
+void UInterpolatorGameFeedbackEffectBase::OnStop(bool bInterrupted)
+{
+	Super::OnStop(bInterrupted);
+
+	Interpolator.UpdateAlpha(GetEffectProgress());
+}
+
+void UInterpolatorGameFeedbackEffectBase::OnTick(float DeltaTime)
+{
+	Super::OnTick(DeltaTime);
+
+	Interpolator.UpdateAlpha(GetEffectProgress());
+}
+
+float UInterpolatorGameFeedbackEffectBase::GetInterpolatorAlpha() const
+{
+	return Interpolator.GetAlpha();
+}
---
+++ Source/GameFeedbackEffect/Public/Common/InterpolatorGameFeedbackEffectBase.h
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Lim Young
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFeedbackEffectBase.h"
+#include "Util/GFUtilStruct.h"
+#include "InterpolatorGameFeedbackEffectBase.generated.h"
+
+/**
+ * Maintains a GFInterpolator instance and updates it with the progress of the effect.
+ * Implementations should call GetInterpolatorAlpha() to get the current alpha value.
+ */
+UCLASS(Abstract)
+class GAMEFEEDBACKEFFECT_API UInterpolatorGameFeedbackEffectBase : public UGameFeedbackEffectBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "Interpolator")
+	FGFInterpolator Interpolator;
+
+protected:
+	virtual void OnInit() override;
+	virtual void OnStop(bool bInterrupted) override;
+	virtual void OnTick(float DeltaTime) override;
+
+protected:
+	UFUNCTION(BlueprintCallable, Category = "GameFeedbackEffect")
+	float GetInterpolatorAlpha() const;
+};
