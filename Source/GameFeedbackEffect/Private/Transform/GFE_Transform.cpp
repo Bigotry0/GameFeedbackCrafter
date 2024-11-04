773a0cee109090ee8ca31082a3a14ffa02a2cef5
Bigotry0 <lim.young@foxmail.com>
2024-11-04 13:44:44 +0800

    Add GFE for Rotation, Scale
    - Fix the bug that GFE_Location did not set the location value according to the interpolator Alpha when stopped

--- Source/GameFeedbackEffect/Private/Transform/GFE_Transform.cpp
+++ Source/GameFeedbackEffect/Private/Transform/GFE_Transform.cpp
@@ -24,7 +24,7 @@ void UGFE_TransformBase::OnInit()
 	}
 }
 
-/// Location
+#pragma region Transform_Location
 void UGFE_Transform_Location::OnPlay()
 {
 	Super::OnPlay();
@@ -46,7 +46,7 @@ void UGFE_Transform_Location::OnStop(bool bInterrupted)
 		return;
 	}
 
-	SetTargetLocation(EndLocation);
+	SetTargetLocation(FMath::Lerp(StartLocation, EndLocation, GetInterpolatorAlpha()));
 }
 
 void UGFE_Transform_Location::OnTick(float DeltaTime)
@@ -58,13 +58,10 @@ void UGFE_Transform_Location::OnTick(float DeltaTime)
 		return;
 	}
 
-	const float Alpha = GetInterpolatorAlpha();
-	const FVector NewLocation = FMath::Lerp(StartLocation, EndLocation, Alpha);
-
-	SetTargetLocation(NewLocation);
+	SetTargetLocation(FMath::Lerp(StartLocation, EndLocation, GetInterpolatorAlpha()));
 }
 
-void UGFE_Transform_Location::SetTargetLocation(FVector NewLocation)
+void UGFE_Transform_Location::SetTargetLocation(const FVector& NewLocation)
 {
 	switch (TransformSpace)
 	{
@@ -100,3 +97,203 @@ void UGFE_Transform_Location::SetTargetLocation(FVector NewLocation)
 		break;
 	}
 }
+#pragma endregion
+
+#pragma region Transform_Rotation
+void UGFE_Transform_Rotation::OnPlay()
+{
+	Super::OnPlay();
+
+	if (!bValid)
+	{
+		return;
+	}
+
+	if (bUseQuatLerp)
+	{
+		StartQuat = StartRotation.Quaternion();
+		EndQuat = EndRotation.Quaternion();
+	}
+	else
+	{
+		StartEuler = StartRotation.Euler();
+		EndEuler = EndRotation.Euler();
+	}
+
+	SetTargetRotation(StartRotation);
+}
+
+void UGFE_Transform_Rotation::OnStop(bool bInterrupted)
+{
+	Super::OnStop(bInterrupted);
+
+	if (!bValid)
+	{
+		return;
+	}
+
+	if (bUseQuatLerp)
+	{
+		FQuat NewQuat;
+		QuatLerp(GetInterpolatorAlpha(), NewQuat);
+
+		SetTargetRotation(NewQuat.Rotator());
+	}
+	else
+	{
+		const FVector NewEuler = FMath::Lerp(StartEuler, EndEuler, GetInterpolatorAlpha());
+		SetTargetRotation(FRotator(NewEuler.Y, NewEuler.Z, NewEuler.X));
+	}
+}
+
+void UGFE_Transform_Rotation::OnTick(float DeltaTime)
+{
+	Super::OnTick(DeltaTime);
+
+	if (!bValid)
+	{
+		return;
+	}
+
+	if (bUseQuatLerp)
+	{
+		FQuat NewQuat;
+		QuatLerp(GetInterpolatorAlpha(), NewQuat);
+
+		SetTargetRotation(NewQuat.Rotator());
+	}
+	else
+	{
+		const FVector NewEuler = FMath::Lerp(StartEuler, EndEuler, GetInterpolatorAlpha());
+		SetTargetRotation(FRotator(NewEuler.Y, NewEuler.Z, NewEuler.X));
+	}
+}
+
+void UGFE_Transform_Rotation::QuatLerp(const float Alpha, FQuat& NewQuat) const
+{
+	switch (RotateLerpMode)
+	{
+	case ERotateLerpMode::FastLerp:
+		NewQuat = FQuat::FastLerp(StartQuat, EndQuat, Alpha);
+		break;
+	case ERotateLerpMode::Slerp:
+		NewQuat = FQuat::Slerp(StartQuat, EndQuat, Alpha);
+		break;
+	default:
+		break;
+	}
+}
+
+void UGFE_Transform_Rotation::SetTargetRotation(const FRotator& NewRotation)
+{
+	switch (TransformSpace)
+	{
+	case ETransformSpace::World:
+		if (bUseCustomTargetComponent)
+		{
+			USceneComponent* TargetComponent = Cast<USceneComponent>(ActorSelection.GetTargetComponent());
+			if (TargetComponent)
+			{
+				TargetComponent->SetWorldRotation(NewRotation);
+			}
+		}
+		else
+		{
+			ActorSelection.GetTargetActor()->SetActorRotation(NewRotation);
+		}
+		break;
+	case ETransformSpace::Local:
+		if (bUseCustomTargetComponent)
+		{
+			USceneComponent* TargetComponent = Cast<USceneComponent>(ActorSelection.GetTargetComponent());
+			if (TargetComponent)
+			{
+				TargetComponent->SetRelativeRotation(NewRotation);
+			}
+		}
+		else
+		{
+			ActorSelection.GetTargetActor()->SetActorRelativeRotation(NewRotation);
+		}
+		break;
+	default:
+		break;
+	}
+}
+#pragma endregion
+
+#pragma region Transform_Scale
+void UGFE_Transform_Scale::OnPlay()
+{
+	Super::OnPlay();
+
+	if (!bValid)
+	{
+		return;
+	}
+
+	SetTargetScale(StartScale);
+}
+
+void UGFE_Transform_Scale::OnStop(bool bInterrupted)
+{
+	Super::OnStop(bInterrupted);
+
+	if (!bValid)
+	{
+		return;
+	}
+
+	SetTargetScale(FMath::Lerp(StartScale, EndScale, GetInterpolatorAlpha()));
+}
+
+void UGFE_Transform_Scale::OnTick(float DeltaTime)
+{
+	Super::OnTick(DeltaTime);
+
+	if (!bValid)
+	{
+		return;
+	}
+
+	SetTargetScale(FMath::Lerp(StartScale, EndScale, GetInterpolatorAlpha()));
+}
+
+void UGFE_Transform_Scale::SetTargetScale(const FVector& NewScale)
+{
+	switch (TransformSpace)
+	{
+	case ETransformSpace::World:
+		if (bUseCustomTargetComponent)
+		{
+			USceneComponent* TargetComponent = Cast<USceneComponent>(ActorSelection.GetTargetComponent());
+			if (TargetComponent)
+			{
+				TargetComponent->SetWorldScale3D(NewScale);
+			}
+		}
+		else
+		{
+			ActorSelection.GetTargetActor()->SetActorScale3D(NewScale);
+		}
+		break;
+	case ETransformSpace::Local:
+		if (bUseCustomTargetComponent)
+		{
+			USceneComponent* TargetComponent = Cast<USceneComponent>(ActorSelection.GetTargetComponent());
+			if (TargetComponent)
+			{
+				TargetComponent->SetRelativeScale3D(NewScale);
+			}
+		}
+		else
+		{
+			ActorSelection.GetTargetActor()->SetActorRelativeScale3D(NewScale);
+		}
+		break;
+	default:
+		break;
+	}
+}
+
+#pragma endregion
--- Source/GameFeedbackEffect/Public/Transform/GFE_Transform.h
+++ Source/GameFeedbackEffect/Public/Transform/GFE_Transform.h
@@ -32,8 +32,16 @@ protected:
 	bool bUseCustomTargetComponent = false;
 
 	virtual void OnInit() override;
+
+#if WITH_EDITOR
+	virtual EGameFeedbackEffectType GetEffectType() const override
+	{
+		return EGameFeedbackEffectType::Transform;
+	}
+#endif
 };
 
+#pragma region Transform_Location
 UCLASS()
 class UGFE_Transform_Location : public UGFE_TransformBase
 {
@@ -51,5 +59,83 @@ protected:
 	virtual void OnTick(float DeltaTime) override;
 
 private:
-	void SetTargetLocation(FVector NewLocation);
+	void SetTargetLocation(const FVector& NewLocation);
 };
+#pragma endregion
+
+#pragma region Transform_Rotation
+UENUM(BlueprintType)
+enum class ERotateMode : uint8
+{
+	Absolute,
+	Additive,
+	ToTarget
+};
+
+UENUM(BlueprintType)
+enum class ERotateLerpMode : uint8
+{
+	FastLerp,
+	Slerp
+};
+
+UCLASS()
+class UGFE_Transform_Rotation : public UGFE_TransformBase
+{
+	GENERATED_BODY()
+
+private:
+	FQuat StartQuat;
+	FQuat EndQuat;
+
+	FVector StartEuler;
+	FVector EndEuler;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Transform|Rotation")
+	ERotateMode RotateMode = ERotateMode::Absolute;
+
+	UPROPERTY(EditAnywhere, Category = "Transform|Rotation")
+	FRotator StartRotation;
+
+	UPROPERTY(EditAnywhere, Category = "Transform|Rotation")
+	FRotator EndRotation;
+
+	UPROPERTY(EditAnywhere, Category = "Transform|Rotation")
+	bool bUseQuatLerp = false;
+
+	UPROPERTY(EditAnywhere, Category = "Transform|Rotation",
+		meta = (EditCondition = "bUseQuatLerp", EditConditionHides = true))
+	ERotateLerpMode RotateLerpMode = ERotateLerpMode::FastLerp;
+
+	virtual void OnPlay() override;
+	virtual void OnStop(bool bInterrupted) override;
+	virtual void OnTick(float DeltaTime) override;
+
+private:
+	void QuatLerp(float Alpha, FQuat& NewQuat) const;
+	void SetTargetRotation(const FRotator& NewRotation);
+};
+#pragma endregion
+
+#pragma region Transform_Scale
+UCLASS()
+class UGFE_Transform_Scale : public UGFE_TransformBase
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Transform|Scale")
+	FVector StartScale = FVector(1.0f);
+
+	UPROPERTY(EditAnywhere, Category = "Transform|Scale")
+	FVector EndScale = FVector(1.0f);
+
+	virtual void OnPlay() override;
+	virtual void OnStop(bool bInterrupted) override;
+	virtual void OnTick(float DeltaTime) override;
+
+private:
+	void SetTargetScale(const FVector& NewScale);
+};
+#pragma endregion
