354282f392b6647116943f86039c6569e3f5bc2a
Bigotry0 <lim.young@foxmail.com>
2024-11-04 13:42:54 +0800

    Fix interpolator effects not being handled differently depending on whether they are interrupted or not when stopped

--- Source/GameFeedbackEffect/Private/Common/InterpolatorGameFeedbackEffectBase.cpp
+++ Source/GameFeedbackEffect/Private/Common/InterpolatorGameFeedbackEffectBase.cpp
@@ -14,7 +14,14 @@ void UInterpolatorGameFeedbackEffectBase::OnStop(bool bInterrupted)
 {
 	Super::OnStop(bInterrupted);
 
-	Interpolator.UpdateAlpha(GetEffectProgress());
+	if (bInterrupted)
+	{
+		Interpolator.UpdateAlpha(GetEffectProgress());
+	}
+	else
+	{
+		Interpolator.UpdateAlpha(1);
+	}
 }
 
 void UInterpolatorGameFeedbackEffectBase::OnTick(float DeltaTime)
