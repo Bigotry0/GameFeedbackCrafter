39b964c6d0e9d181b7466abd7abada2b8c86fe16
Bigotry0 <lim.young@foxmail.com>
2024-11-06 00:09:34 +0800

    Add GFNoise util structure, based on FastNoise library

---
+++ Source/GameFeedbackCore/Private/Util/GFNoise.cpp
@@ -0,0 +1,101 @@
+﻿// Copyright (c) Lim Young
+
+
+#include "Util/GFNoise.h"
+
+FGFNoise::FGFNoise()
+{
+	UpdateNoise();
+}
+
+float FGFNoise::GetNoise1D(const float Alpha) const
+{
+	float X, Y;
+	CalculateSamplePosition(Alpha, X, Y);
+
+	return Noise->GetNoise(X, Y) * Scale * !bIgnoreX;
+}
+
+FVector2d FGFNoise::GetNoise2D(const float Alpha) const
+{
+	float X, Y;
+	CalculateSamplePosition(Alpha, X, Y);
+
+	return FVector2d(Noise->GetNoise(X, Y) * !bIgnoreX, Noise->GetNoise(Y, X) * !bIgnoreY) * Scale;
+}
+
+FVector FGFNoise::GetNoise3D(const float Alpha) const
+{
+	float X, Y, Z;
+	CalculateSamplePosition(Alpha, X, Y, Z);
+
+	return FVector(Noise->GetNoise(X, Y) * !bIgnoreX, Noise->GetNoise(Y, X) * !bIgnoreY,
+	               Noise->GetNoise(0.0f, 0.0f, Z) * !bIgnoreZ) * Scale;
+}
+
+void FGFNoise::UpdateSampleDirection()
+{
+	SampleDirection = FVector2d(FMath::Cos(SampleDirectionOffsetAngle), FMath::Sin(SampleDirectionOffsetAngle));
+}
+
+void FGFNoise::UpdateNoiseType() const
+{
+	switch (NoiseType)
+	{
+	case EGFNoiseType::OpenSimplex2:
+		Noise->SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2);
+		break;
+	case EGFNoiseType::OpenSimplex2S:
+		Noise->SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2S);
+		break;
+	case EGFNoiseType::Cellular:
+		Noise->SetNoiseType(FastNoiseLite::NoiseType_Cellular);
+		break;
+	case EGFNoiseType::Perlin:
+		Noise->SetNoiseType(FastNoiseLite::NoiseType_Perlin);
+		break;
+	case EGFNoiseType::ValueCubic:
+		Noise->SetNoiseType(FastNoiseLite::NoiseType_ValueCubic);
+		break;
+	case EGFNoiseType::Value:
+		Noise->SetNoiseType(FastNoiseLite::NoiseType_Value);
+		break;
+	}
+}
+
+void FGFNoise::UpdateNoise()
+{
+	if (bUseRandomSeed)
+	{
+		Noise->SetSeed(FMath::Rand());
+	}
+	else
+	{
+		Noise->SetSeed(Seed);
+	}
+
+	UpdateNoiseType();
+	UpdateSampleDirection();
+}
+
+void FGFNoise::CalculateSamplePosition(const float Alpha, float& X, float& Y) const
+{
+	float SampleAccuracyHalf = SampleAccuracy * 0.5;
+	FVector2d SampleStart = -SampleDirection * 0.5 * SampleAccuracyHalf;
+	FVector2d SampleEnd = SampleDirection * 0.5 * SampleAccuracyHalf;
+
+	X = FMath::Lerp(SampleStart.X, SampleEnd.X, Alpha);
+	Y = FMath::Lerp(SampleStart.Y, SampleEnd.Y, Alpha);
+}
+
+void FGFNoise::CalculateSamplePosition(const float Alpha, float& X, float& Y, float& Z) const
+{
+	float SampleAccuracyHalf = SampleAccuracy * 0.5;
+	FVector2d SampleStart = -SampleDirection * 0.5 * SampleAccuracyHalf;
+	FVector2d SampleEnd = SampleDirection * 0.5 * SampleAccuracyHalf;
+
+	X = FMath::Lerp(SampleStart.X, SampleEnd.X, Alpha);
+	Y = FMath::Lerp(SampleStart.Y, SampleEnd.Y, Alpha);
+
+	Z = FMath::Lerp(-0.5f, 0.5f, Alpha) * SampleAccuracyHalf;
+}
---
+++ Source/GameFeedbackCore/Public/Util/GFNoise.h
@@ -0,0 +1,113 @@
+﻿// Copyright (c) Lim Young
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ThirdParty/FastNoiseLite.h"
+#include "GFNoise.generated.h"
+
+UENUM(BlueprintType)
+enum class EGFNoiseType : uint8
+{
+	OpenSimplex2,
+	OpenSimplex2S,
+	Cellular,
+	Perlin,
+	ValueCubic,
+	Value
+};
+
+/**
+ * Noise generator.
+ */
+USTRUCT(BlueprintType)
+struct GAMEFEEDBACKCORE_API FGFNoise
+{
+	GENERATED_BODY()
+
+private:
+	TSharedPtr<FastNoiseLite> Noise = MakeShared<FastNoiseLite>();
+
+public:
+	FGFNoise();
+
+protected:
+	/**
+	 * Noise type.
+	 */
+	UPROPERTY(EditAnywhere, Category = "Noise")
+	EGFNoiseType NoiseType = EGFNoiseType::Perlin;
+
+	/**
+	 * Whether to use a random seed.
+	 */
+	UPROPERTY(EditAnywhere, Category = "Noise")
+	bool bUseRandomSeed = true;
+
+	/**
+	 * Seed value. Default is 1337.
+	 */
+	UPROPERTY(EditAnywhere, Category = "Noise", meta = (EditCondition = "!bUseRandomSeed", EditConditionHides = true))
+	int32 Seed = 1337;
+
+	/**
+	 * The size of the generated noise.
+	 * When tuning the parameter, it can be simply understood that the larger the value, the higher the frequency.
+	 * And the smaller the value, the lower the frequency.
+	 */
+	UPROPERTY(EditAnywhere, Category = "Noise")
+	int32 SampleAccuracy = 64;
+
+	/**
+	 * The direction of the noise sample.
+	 * This value generally does not need to be adjusted,
+	 * and directly adjusting this value is in fact basically equivalent to directly adjusting the noise seed.
+	 * 
+	 * TODO: Slide this value to achieve special noise effects
+	 */
+	UPROPERTY(EditAnywhere, Category = "Noise", meta = (UIMin = "-360.0", UIMax = "360.0"))
+	float SampleDirectionOffsetAngle = 0;
+
+	/**
+	 * The scale of the noise.
+	 */
+	UPROPERTY(EditAnywhere, Category = "Noise")
+	float Scale = 1.0f;
+
+	/**
+	 * Whether to ignore the X when return the result.
+	 */
+	UPROPERTY(EditAnywhere, AdvancedDisplay, Category = "Noise")
+	bool bIgnoreX = false;
+
+	/**
+	 * Whether to ignore the Y when return the result.
+	 */
+	UPROPERTY(EditAnywhere, AdvancedDisplay, Category = "Noise")
+	bool bIgnoreY = false;
+
+	/**
+	 * Whether to ignore the Z when return the result.
+	 */
+	UPROPERTY(EditAnywhere, AdvancedDisplay, Category = "Noise")
+	bool bIgnoreZ = false;
+
+private:
+	FVector2d SampleDirection;
+
+public:
+	float GetNoise1D(const float Alpha) const;
+	FVector2d GetNoise2D(const float Alpha) const;
+	FVector GetNoise3D(const float Alpha) const;
+
+private:
+	void UpdateSampleDirection();
+	void UpdateNoiseType() const;
+
+public:
+	void UpdateNoise();
+
+private:
+	void CalculateSamplePosition(const float Alpha, float& X, float& Y) const;
+	void CalculateSamplePosition(const float Alpha, float& X, float& Y, float& Z) const;
+};
