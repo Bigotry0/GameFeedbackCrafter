019a903df7520d04036b861f816dbc0adea60813
Bigotry0 <lim.young@foxmail.com>
2024-11-06 01:11:13 +0800

    Add Two Time Game Feedback Effects

---
+++ Source/GameFeedbackEffect/Private/Time/GFE_Time.cpp
@@ -0,0 +1,89 @@
+﻿// Copyright (c) Lim Young
+
+
+#include "Time/GFE_Time.h"
+
+#include "Kismet/GameplayStatics.h"
+
+void UGFE_Time_FreezeFrame::OnInit()
+{
+	Super::OnInit();
+
+	ContextWorld = GetContextObject()->GetWorld();
+}
+
+void UGFE_Time_FreezeFrame::OnPlay()
+{
+	Super::OnPlay();
+
+	if (!ContextWorld)
+	{
+		return;
+	}
+
+	OriginalTimeDilation = UGameplayStatics::GetGlobalTimeDilation(ContextWorld);
+	UGameplayStatics::SetGlobalTimeDilation(ContextWorld, MinimumTimeDilation);
+}
+
+void UGFE_Time_FreezeFrame::OnStop(bool bInterrupted)
+{
+	Super::OnStop(bInterrupted);
+
+	if (!ContextWorld)
+	{
+		return;
+	}
+
+	UGameplayStatics::SetGlobalTimeDilation(ContextWorld, OriginalTimeDilation);
+}
+
+void UGFE_Time_TimeFollow::OnInit()
+{
+	Super::OnInit();
+
+	ContextWorld = GetContextObject()->GetWorld();
+}
+
+void UGFE_Time_TimeFollow::OnPlay()
+{
+	Super::OnPlay();
+
+	if (!ContextWorld)
+	{
+		return;
+	}
+
+	UGameplayStatics::SetGlobalTimeDilation(ContextWorld, StartTimeDilation);
+}
+
+void UGFE_Time_TimeFollow::OnTick(float DeltaTime)
+{
+	Super::OnTick(DeltaTime);
+
+	if (!ContextWorld)
+	{
+		return;
+	}
+
+	UGameplayStatics::SetGlobalTimeDilation(ContextWorld, FMath::Lerp(StartTimeDilation, EndTimeDilation,
+	                                                                  GetInterpolatorAlpha()));
+}
+
+void UGFE_Time_TimeFollow::OnStop(bool bInterrupted)
+{
+	Super::OnStop(bInterrupted);
+
+	if (!ContextWorld)
+	{
+		return;
+	}
+
+	if (bInterrupted && bForceEndTimeDilationWhenInterrupted)
+	{
+		UGameplayStatics::SetGlobalTimeDilation(ContextWorld, EndTimeDilation);
+		return;
+	}
+
+	UGameplayStatics::SetGlobalTimeDilation(ContextWorld, FMath::Lerp(StartTimeDilation, EndTimeDilation,
+	                                                                  GetInterpolatorAlpha()));
+}
---
+++ Source/GameFeedbackEffect/Public/Time/GFE_Time.h
@@ -0,0 +1,82 @@
+﻿// Copyright (c) Lim Young
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFeedbackEffectBase.h"
+#include "Common/GFE_InterpolatorGameFeedbackEffectBase.h"
+#include "GFE_Time.generated.h"
+
+
+UCLASS()
+class UGFE_Time_FreezeFrame : public UGameFeedbackEffectBase
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * The time dilation value to apply during the freeze-frame effect.
+	 */
+	UPROPERTY(EditAnywhere, Category = "Time")
+	float MinimumTimeDilation = 0.1f;
+
+private:
+	float OriginalTimeDilation = 1.0f;
+
+	UPROPERTY()
+	UWorld* ContextWorld = nullptr;
+
+protected:
+	virtual void OnInit() override;
+	virtual void OnPlay() override;
+	virtual void OnStop(bool bInterrupted) override;
+
+#if WITH_EDITOR
+	virtual EGameFeedbackEffectType GetEffectType() const override
+	{
+		return EGameFeedbackEffectType::Time;
+	}
+#endif
+};
+
+UCLASS()
+class UGFE_Time_TimeFollow : public UGFE_InterpolatorGameFeedbackEffectBase
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * Time dilation coefficients for interpolated starting points
+	 */
+	UPROPERTY(EditAnywhere, Category = "Time")
+	float StartTimeDilation = 0.1f;
+
+	/**
+	 * Time dilation coefficients for interpolated ending points
+	 */
+	UPROPERTY(EditAnywhere, Category = "Time")
+	float EndTimeDilation = 1.0f;
+
+	/**
+	 * Force time dilation to EndTimeDilation when interrupted
+	 */
+	UPROPERTY(EditAnywhere, Category = "Time")
+	bool bForceEndTimeDilationWhenInterrupted = false;
+
+private:
+	UPROPERTY()
+	UWorld* ContextWorld = nullptr;
+
+protected:
+	virtual void OnInit() override;
+	virtual void OnPlay() override;
+	virtual void OnTick(float DeltaTime) override;
+	virtual void OnStop(bool bInterrupted) override;
+
+#if WITH_EDITOR
+	virtual EGameFeedbackEffectType GetEffectType() const override
+	{
+		return EGameFeedbackEffectType::Time;
+	}
+#endif
+};
